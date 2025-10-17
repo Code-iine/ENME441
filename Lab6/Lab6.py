d288bdfb4a7bb9c00e2c110a2b4d7b884e9cbaca
mgomes <mgomes13@terpmail.umd.edu>
2025-10-16 23:10:10 -0400

    created a walk

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -1,6 +1,9 @@
 from shifter import Shifter
 import time
 import RPi.GPIO as GPIO
+import random
+
+range = [1, -1]
 
 serialPin = 23
 latchPin = 24
@@ -10,10 +13,27 @@ Shifter1 = Shifter(serialPin,latchPin,clockPin)
 
 try:
   print("testing")
+  initial = 5
   while 1:
+    
+    step = random.choice(range)
+    walk = initial + step
+
+    if walk > 0 and walk < 8:
+      initial = walk
+    else:
+      initial = 4
+
+    Shifter1.shiftByte(walk)
+
+
+
+
+    '''
     for i in range(108):
         Shifter1.shiftByte(i)
         time.sleep(0.5)
+        '''
 except:
   GPIO.cleanup()
 
