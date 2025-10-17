e7e55eebbbcca5ff58e4d2b56a2a0f1a8614db6d
mgomes <mgomes13@terpmail.umd.edu>
2025-10-16 22:54:15 -0400

    changed range in lab6

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -11,12 +11,12 @@ Shifter1 = Shifter(serialPin,latchPin,clockPin)
 try:
   print("testing")
   while 1:
-    for i in range(2**8):
+    for i in range(108):
         Shifter1.shiftByte(i)
         time.sleep(0.5)
 except:
   GPIO.cleanup()
 
-finally:
+finally: #Turn off all pins
    Shifter1.shiftByte(0)
    GPIO.cleanup()
