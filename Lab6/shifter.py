d760af940a522be01e4795091918a59e6b42c718
mgomes <mgomes13@terpmail.umd.edu>
2025-10-19 14:51:32 -0400

    modified wrap code

--- Lab6/shifter.py
+++ Lab6/shifter.py
@@ -48,12 +48,15 @@ class Bug:
             walk = self.x + step
 
             if self.isWrapOn == True:
+                self.x = walk % 8
+                '''
                 if self.x > 8:
                     self.x = 1
                 elif self.x < 0:
                     self.x = 8
+                    '''
             else:
-                if walk > 0 and walk < 8:
+                if walk > 0 and walk < 7:
                     self.x = walk
 
             self.__shifter.shiftByte(1<<self.x)
