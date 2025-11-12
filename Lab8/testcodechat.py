6bbdfd9707f6d41d89e67ec6c1e4fb444c5cf258
mgomes <mgomes13@terpmail.umd.edu>
2025-11-12 00:00:52 -0500

    added comments back

--- Lab8/testcodechat.py
+++ Lab8/testcodechat.py
@@ -1,4 +1,3 @@
-
 import time
 import multiprocessing
 from shifter import Shifter   # our custom Shifter class
@@ -26,38 +25,45 @@ class Stepper:
     """
 
     # Class attributes:
-    num_steppers = 0                #track number of Steppers instantiated
+    num_steppers = 0                # track number of Steppers instantiated
     shifter_outputs = multiprocessing.Value('I', 0)  # track shift register outputs for all motors
-    seq = [0b0001, 0b0011, 0b0010, 0b0110, 0b0100, 0b1100, 0b1000, 0b1001]  # CCW sequence
+    seq = [0b0001, 0b0011, 0b0010, 0b0110,
+           0b0100, 0b1100, 0b1000, 0b1001]  # CCW sequence
     delay = 1200  # delay between motor steps [us]
-    steps_per_degree = 4096 / 360 # 4096 steps/rev * 1/360 rev/deg
+    steps_per_degree = 4096 / 360  # 4096 steps/rev * 1/360 rev/deg
 
     def __init__(self, shifter, lock):
-        self.s = shifter         # shift register
-        self.angle = 0          # current output shaft angle
-        self.step_state = 0        # track position in sequence
-        self.start_bit = 4 * Stepper.num_steppers # starting bit position
-        self.lock = lock            # multiprocessing lock
-        Stepper.num_steppers += 1        # increment the instance count
+        self.s = shifter          # shift register
+        self.angle = 0.0          # current output shaft angle
+        self.step_state = 0       # track position in sequence
+        self.start_bit = 4 * Stepper.num_steppers  # starting bit position
+        self.lock = lock          # multiprocessing lock
+        Stepper.num_steppers += 1 # increment the instance count
 
-     # Signum function:
+    # Signum function:
     def __sgn(self, x):
-        if x == 0: return(0)
-        else: return(int(abs(x)/x))
+        if x == 0:
+            return 0
+        else:
+            return int(x / abs(x))
 
     # Move a single +/-1 step in the motor sequence:
     def __step(self, direction):
         # one step in given direction
+        # reverse sequence if needed for opposite motor
+        if (self.start_bit // 4) % 2 == 1:  
+            direction = -direction  # alternate motors spin opposite ways
+
         self.step_state = (self.step_state + direction) % 8
         pattern = Stepper.seq[self.step_state] << self.start_bit
         mask = 0b1111 << self.start_bit
 
         with self.lock:
             cur = Stepper.shifter_outputs.value
-            cur &= ~mask          # clear this motor's 4 bits
-            cur |= pattern        # set new pattern
+            cur &= ~mask           # clear this motor's 4 bits
+            cur |= pattern         # set new pattern
             Stepper.shifter_outputs.value = cur
-            self.s.shiftByte(cur) # send to shift register
+            self.s.shiftByte(cur)  # send to shift register
 
         self.angle = (self.angle + direction / Stepper.steps_per_degree) % 360
         time.sleep(Stepper.delay / 1e6)
@@ -98,7 +104,7 @@ if __name__ == '__main__':
     m1.goAngle(0)
     m2.goAngle(45)
     '''
-    #Problem 3 directions
+    # Problem 3 directions
     m1.zero()
     m2.zero()
     m1.goAngle(90)
